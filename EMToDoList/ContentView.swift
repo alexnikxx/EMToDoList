39ce6dd991dd9fe8a51515dd60f8bdcbe85e03c8
alexnikxx <alexnikxx@gmail.com>
2024-11-14 21:42:25 +0300

    Initial Commit

---
+++ EMToDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EMToDoList
+//
+//  Created by Aleksandra Nikiforova on 14/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EMToDoList/EMToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMToDoListApp.swift
+//  EMToDoList
+//
+//  Created by Aleksandra Nikiforova on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EMToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
