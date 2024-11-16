b819a71120cdc138bcc28b2ea1511dfcd98db873
alexnikxx <alexnikxx@gmail.com>
2024-11-16 15:29:28 +0300

    Initial Commit

---
+++ EMToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EMToDoList
+//
+//  Created by Aleksandra Nikiforova on 16/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
