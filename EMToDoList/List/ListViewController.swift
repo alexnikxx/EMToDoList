fdc4b7cb3c93bd66bf2e1be6cb0a4f6cc750bbf0
alexnikxx <alexnikxx@gmail.com>
2024-11-29 20:48:38 +0300

    Fix search results and refine code

--- EMToDoList/List/ListViewController.swift
+++ EMToDoList/List/ListViewController.swift
@@ -11,6 +11,8 @@ class ListViewController: UIViewController, ListViewProtocol {
     let configurator = ListConfigurator()
     var presenter: ListPresenterProtocol?
 
+    //MARK: Main properties
+
     private var list: [CustomTodo] = []
     private var filteredList: [CustomTodo] = []
 
@@ -18,6 +20,7 @@ class ListViewController: UIViewController, ListViewProtocol {
         let tableView = UITableView()
         tableView.backgroundColor = .todoBlack
         tableView.allowsSelection = true
+        tableView.showsVerticalScrollIndicator = false
         tableView.register(ListTableViewCell.self, forCellReuseIdentifier: ListTableViewCell.identifier)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
@@ -38,25 +41,35 @@ class ListViewController: UIViewController, ListViewProtocol {
         return label
     }()
 
-    private let searchController = UISearchController(searchResultsController: nil)
+    //MARK: Search controller properties
+
+    private let searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        let textField = searchController.searchBar.searchTextField
+        textField.backgroundColor = .todoGray
+        return searchController
+    }()
+
+    private var isSearchBarEmpty: Bool {
+      return searchController.searchBar.text?.isEmpty ?? true
+    }
+
+    private var isFiltering: Bool {
+        return searchController.isActive && !isSearchBarEmpty
+    }
+
+    //MARK: Main functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configurator.configure(for: self)
-        activityIndicator.startAnimating()
+        showLoadingIndicator()
         presenter?.appStarts()
 
         setupView()
+        setupNavBarAndToolbar()
         setupSearchController()
         setupActivityIndicator()
-
-        navigationController?.toolbar.isTranslucent = false
-        navigationController?.setToolbarHidden(false, animated: false)
-        navigationController?.toolbar.barTintColor = .todoGray
-        navigationController?.toolbar.tintColor = .todoWhite
-
-        tableView.delegate = self
-        tableView.dataSource = self
     }
 
     func displayLoadedData(customTodos: [CustomTodo]) {
@@ -66,39 +79,26 @@ class ListViewController: UIViewController, ListViewProtocol {
         count.text = "\(list.count) задач"
     }
 
-    @objc func actionTapped() {
-        print("Action tapped")
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+
+        setupToolbar()
     }
 
     @objc func newTaskButtonTapped() {
         presenter?.newTodoButtonTapped()
     }
 
-    func updateCount() -> Int {
-        let count = 0
-        return count
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        navigationItem.title = "Задачи"
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.largeTitleDisplayMode = .always
-
-        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-
-        let tasksCountLabel = UIBarButtonItem(customView: self.count)
-
-        let newTaskButton = UIBarButtonItem(title: nil, style: .plain, target: self, action: #selector(newTaskButtonTapped))
-        newTaskButton.image = UIImage(systemName: "square.and.pencil")
-        newTaskButton.tintColor = .todoYellow
-
-        toolbarItems = [flexibleSpace, tasksCountLabel, flexibleSpace, newTaskButton]
-        setToolbarItems(toolbarItems, animated: false)
-    }
+    //MARK: Setup view functions
 
     private func setupView() {
+        navigationItem.title = "Задачи"
         view.backgroundColor = .todoBlack
         view.addSubview(tableView)
+
+        tableView.delegate = self
+        tableView.dataSource = self
 
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -108,31 +108,45 @@ class ListViewController: UIViewController, ListViewProtocol {
         ])
     }
 
-    private func setupSearchController() {
-        searchController.searchResultsUpdater = self
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.hidesNavigationBarDuringPresentation = false
-        searchController.searchBar.placeholder = "Search"
+    private func setupNavBarAndToolbar() {
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barTintColor = .todoGray
 
+        navigationController?.toolbar.isTranslucent = false
+        navigationController?.setToolbarHidden(false, animated: false)
+        navigationController?.toolbar.barTintColor = .todoGray
+        navigationController?.toolbar.tintColor = .todoYellow
+    }
+
+    private func setupToolbar() {
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+
+        let tasksCountLabel = UIBarButtonItem(customView: self.count)
+
+        let newTaskButton = UIBarButtonItem(title: nil, style: .plain, target: self, action: #selector(newTaskButtonTapped))
+        newTaskButton.image = UIImage(systemName: "square.and.pencil")
+
+        toolbarItems = [flexibleSpace, tasksCountLabel, flexibleSpace, newTaskButton]
+        setToolbarItems(toolbarItems, animated: false)
+    }
+
+    //MARK: Search Controller functions
+
+    private func setupSearchController() {
         navigationItem.searchController = searchController
-        definesPresentationContext = false
         navigationItem.hidesSearchBarWhenScrolling = false
 
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.automaticallyShowsSearchResultsController = true
+
         let searchBar = searchController.searchBar
-        searchBar.delegate = self
         let audioImage = UIImage(systemName: "mic.fill")
         searchBar.setImage(audioImage, for: .bookmark, state: [.normal])
         searchBar.showsBookmarkButton = true
     }
 
-    func inSearchMode(_ searchController: UISearchController) -> Bool {
-        let isActive = searchController.isActive
-        let searchText = searchController.searchBar.text ?? ""
-
-        return isActive && !searchText.isEmpty
-    }
-
-    func updateSearchController(searchBarText: String?) {}
+    //MARK: Activity indicator functions
 
     private func showLoadingIndicator() {
         activityIndicator.startAnimating()
@@ -152,19 +166,31 @@ class ListViewController: UIViewController, ListViewProtocol {
     }
 }
 
-extension ListViewController: UISearchResultsUpdating, UISearchBarDelegate {
+//MARK: Search Results Updates
+
+extension ListViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
         guard let text = searchController.searchBar.text else { return }
-        print(text)
+        filterTodos(with: text)
     }
 
-    func searchBarBookmarkButtonClicked(_ searchBar: UISearchBar) {
-        print("Tapped!")
+    private func filterTodos(with text: String) {
+        filteredList = list.filter { (todo: CustomTodo) -> Bool in
+            return todo.title.lowercased().contains(text.lowercased())
+        }
+
+        tableView.reloadData()
     }
 }
 
+//MARK: Setup Table View
+
 extension ListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering {
+            return filteredList.count
+        }
+
         return list.count
     }
 
@@ -173,7 +199,13 @@ extension ListViewController: UITableViewDelegate, UITableViewDataSource {
             fatalError("The table view could not dequeue a custom cell.")
         }
 
-        let todo = list[indexPath.row]
+        let todo: CustomTodo
+        if isFiltering {
+            todo = filteredList[indexPath.row]
+        } else {
+            todo = list[indexPath.row]
+        }
+
         cell.update(todo: todo)
 
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapEdit(recognizer:)))
@@ -183,7 +215,13 @@ extension ListViewController: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let todo = list[indexPath.row]
+        let todo: CustomTodo
+        if isFiltering {
+            todo = filteredList[indexPath.row]
+        } else {
+            todo = list[indexPath.row]
+        }
+
         let detailView = DetailViewController(todo: todo, saveTodo: { [weak self] updatedTodo in
             self?.list[indexPath.row] = updatedTodo
             self?.tableView.reloadData()
